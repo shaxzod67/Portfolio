dc8c579ff16d45701ec88f3d29ddd9b154dd6491
sardor1411 <shahnozka1104@gmail.com>
2024-04-02 12:20:19 +0500

    new dark

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,12 +1,34 @@
 let btn = document.querySelector('#btn');
 let moon = document.querySelector('#tun');
+let moon1 = document.querySelector('#moon1');
+let sun = document.querySelector('#sun');
 
 function dark (){
     if(moon.getAttribute('href') == './css/style.css'){
         moon.href = './css/app.css'
+         sun.style.display = "block"
+         moon1.style.display = "none"
     }
     else{
         moon.href = './css/style.css'
+        sun.style.display = "block"
+        moon1.style.display = "none"
     }
+
 }
-btn.onclick = dark
+moon1.onclick = dark
+
+function dark1 (){
+    if(moon.getAttribute('href') == './css/app.css'){
+        moon.href = './css/style.css'
+        sun.style.display = "none"
+        moon1.style.display = "block"
+    }
+    else{
+        moon.href = './css/app.css'
+        sun.style.display = "none"
+         moon1.style.display = "block"
+    }
+
+}
+sun.onclick = dark1
